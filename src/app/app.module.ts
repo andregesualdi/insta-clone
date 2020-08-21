7b17b1c3d0c37baddc647e48763fb149427624f3
Andre Gesualdi <andrergesualdi@gmail.com>
2020-08-20 22:51:04 -0300

    Commit inicial: Primeiras configurações

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AcessoComponent } from './core/components/acesso/acesso.component';
+import { BannerComponent } from './core/components/acesso/banner/banner.component';
+import { LoginComponent } from './core/components/acesso/login/login.component';
+import { CadastroComponent } from './core/components/acesso/cadastro/cadastro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AcessoComponent,
+    BannerComponent,
+    LoginComponent,
+    CadastroComponent
   ],
   imports: [
     BrowserModule
